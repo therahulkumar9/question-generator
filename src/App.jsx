a28d48aff0f19d3ad6880fd328a4d750346c76b9
Rahul Kumar <therahulkumar135@gmail.com>
2025-01-09 19:07:02 +0530

    Added styling and jsx for container of input

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
     <>
       <div className='main-container'>
       <h1 className='heading'>AI Question Generator</h1>
+      <div className="form-container">
+        <div>
+          <label htmlFor='query'>Enter Query: </label>
+          <input type="text" name="" id="query" />
+        </div>
+      </div>
       </div>
     </>
   )
